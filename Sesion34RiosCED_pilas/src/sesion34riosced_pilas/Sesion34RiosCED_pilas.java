6e67c67a9f1759ae77fef9170b4005b424d41a80
Abigail <ariosabi@gmail.com>
2024-10-28 15:36:37 -0600

    Confirmacion 1 OK Rios
    Datos de desarrollo: Abigail Ríos

---
+++ Sesion34RiosCED_pilas/src/sesion34riosced_pilas/Sesion34RiosCED_pilas.java
@@ -0,0 +1,26 @@
+package sesion34riosced_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Abigail
+ */
+public class Sesion34RiosCED_pilas {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declaración de pila
+        Stack<Integer> pilaRios = new Stack<>();
+        pilaRios.push(100);
+        pilaRios.push(150);
+        pilaRios.push(230);
+        pilaRios.push(10);
+        pilaRios.push(160);
+        
+        JOptionPane.showMessageDialog(null, "Datos en pila ordenada es: \n" + pilaRios);
+    }
+    
+}
